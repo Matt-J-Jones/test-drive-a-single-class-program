a966c228fb6c16a222c540d54d301df1e690ad11
Matt Jones <67526111+Matt-J-Jones@users.noreply.github.com>
2023-03-16 18:02:10 +0000

    Challenge Complete

--- lib/grammar_stats.rb
+++ lib/grammar_stats.rb
@@ -2,16 +2,41 @@
 
 class GrammarStats
   def initialize
-    # ...
+    @count = 0
+    @passes = 0
   end
 
-  def check(text) # text is a string
-    # Returns true or false depending on whether the text begins with a capital
-    # letter and ends with a sentence-ending punctuation mark.
+  def check(text) 
+    # increases count of texts checked
+    @count += 1
+
+    # sets default to false, and if text meets condition
+    # returns true.
+    to_check = text[0].upcase
+    capital_used = false; 
+    capital_used = true if text[0] == to_check[0]
+
+    # sets default to false, and if text meets condition
+    # returns true.
+    punct = ".?!"; punct_used = false; 
+    punct_used = true if punct.include?(text[text.length-1])
+
+    # if both tests return true, the method returns true
+    if capital_used == true && punct_used == true
+      # increases count of passed tests
+      @passes += 1
+      return true
+    else
+      return false
+    end
   end
 
   def percentage_good
-    # Returns as an integer the percentage of texts checked so far that passed
-    # the check defined in the `check` method. The number 55 represents 55%.
+    # calculates percentage of the overall tests that have 
+    # passed, needs to convert into float to work out percentage
+    # before being returned as in int.
+    
+    percentage = ((@passes * 1.0) / (@count * 1.0)) * 100
+    return percentage.to_i
   end
 end
--- spec/grammar_stats_spec.rb
+++ spec/grammar_stats_spec.rb
@@ -1,3 +1,59 @@
 # File: spec/gramma_stats_spc.rb
 require 'grammar_stats'
 
+RSpec.describe GrammarStats do
+  it "returns true with input 'Lorem ipsum dolor sit amet.'" do
+    stats = GrammarStats.new
+    result = stats.check("Lorem ipsum dolor sit amet.")
+    expect(result).to eq true
+  end
+
+  it "returns false with input 'lorem ipsum dolor sit amet'" do
+    stats = GrammarStats.new
+    result = stats.check("lorem ipsum dolor sit amet")
+    expect(result).to eq false
+  end
+
+  it "returns false with input 'Lorem ipsum dolor sit amet'" do
+    stats = GrammarStats.new
+    result = stats.check("Lorem ipsum dolor sit amet")
+    expect(result).to eq false
+  end
+
+  it "returns false with input 'lorem ipsum dolor sit amet.'" do
+    stats = GrammarStats.new
+    result = stats.check("Lorem ipsum dolor sit amet")
+    expect(result).to eq false
+  end
+
+  it "returns 50% pass rate when two tests ran (pass, fail)" do
+    stats = GrammarStats.new
+    stats.check("Lorem ipsum dolor sit amet.")
+    stats.check("lorem ipsum dolor sit amet")
+    expect(stats.percentage_good).to eq 50
+  end
+
+  it "returns 77% pass rate when 100 tests ran (77 pass, 23 fail)" do
+    stats = GrammarStats.new
+    77.times {stats.check("Lorem ipsum dolor sit amet.")}
+    23.times {stats.check("lorem ipsum dolor sit amet")}
+    expect(stats.percentage_good).to eq 77
+  end
+
+  # when converting float to int, number is round down.
+  # so 12.5% becomes 12%
+  it "returns 12% pass rate when 100 tests ran (25 pass, 175 fail)" do
+    stats = GrammarStats.new
+    25.times {stats.check("Lorem ipsum dolor sit amet.")}
+    175.times {stats.check("lorem ipsum dolor sit amet")}
+    expect(stats.percentage_good).to eq 12
+  end
+
+  it "returns 33% pass rate when 100 tests ran (33 pass, 67 fail)" do
+    stats = GrammarStats.new
+    33.times {stats.check("Lorem ipsum dolor sit amet.")}
+    33.times {stats.check("lorem ipsum dolor sit amet")}
+    34.times {stats.check("lorem ipsum dolor sit amet.")}
+    expect(stats.percentage_good).to eq 33
+  end
+end
